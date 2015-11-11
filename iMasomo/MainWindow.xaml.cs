40cf52dc3ac8798bd65f6c130cc83515f284cc37
Denson Ngumo <davincingumo@gmail.com>
2015-11-11 21:01:11 +0300

    first commit,started on vocabularies module

---
+++ iMasomo/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+
+namespace iMasomo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuTileList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedTile = menuTileList.SelectedItem as Tile;
+           // MessageBox.Show(selectedTile.Name);
+            Frame myFrame= new Frame();
+            EnglishMenu englishMenu= new EnglishMenu();
+            KiswahiliMenu kiswahiliMenu = new KiswahiliMenu();
+            switch(selectedTile.Name)
+            {
+                case "englishTile":
+                    myFrame.Content = englishMenu;
+                    mainWindow.Content = myFrame;
+                    break;
+                case "kiswahiliTile":
+                    myFrame.Content = kiswahiliMenu;
+                    mainWindow.Content = myFrame;
+                    break;
+            }
+        }
+    }
+}
---
+++ iMasomo/Vocabularies.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace iMasomo
+{
+    /// <summary>
+    /// Interaction logic for Vocabularies.xaml
+    /// </summary>
+    public partial class Vocabularies : Window
+    {
+        SortedList<String, BitmapImage> imageCollection = new SortedList<string, BitmapImage>();
+        string systemImagesPath = Environment.CurrentDirectory;
+
+        private int currImage = 0;
+        private int MAXIMAGE = 6;
+
+        public Vocabularies()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //load system images
+            imageCollection.Add("cat", new BitmapImage(new Uri(String.Format(@"/Images/cat.jpg"),UriKind.Relative)));
+            imageCollection.Add("fish", new BitmapImage(new Uri(String.Format(@"/Images/fish.jpg"), UriKind.Relative)));
+            imageCollection.Add("footballer", new BitmapImage(new Uri(String.Format(@"/Images/footballer.jpg"), UriKind.Relative)));
+            imageCollection.Add("leaves", new BitmapImage(new Uri(String.Format(@"/Images/leaves.jpg"), UriKind.Relative)));
+            imageCollection.Add("pumpkins", new BitmapImage(new Uri(String.Format(@"/Images/pumpkins.jpg"), UriKind.Relative)));
+            imageCollection.Add("snow-leopard", new BitmapImage(new Uri(String.Format(@"/Images/snow-leopard.jpg"), UriKind.Relative)));
+            imageCollection.Add("tree", new BitmapImage(new Uri(String.Format(@"/Images/tree.jpg"), UriKind.Relative)));
+            imageHolder.Source = imageCollection.Values[0];
+
+        }
+
+       
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (--currImage < 0)
+                currImage = MAXIMAGE;
+            imageHolder.Source = imageCollection.Values[currImage];
+
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (++currImage > MAXIMAGE)
+                currImage = 0;
+            imageHolder.Source = imageCollection.Values[currImage];
+        }
+    }
+}
