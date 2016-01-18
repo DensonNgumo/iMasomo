cde3360b35d09dd9bcfe93cf83a4cdac215fd28d
Denson Ngumo <davincingumo@gmail.com>
2016-01-18 13:23:36 +0300

    Added a homepage

---
+++ iMasomo Teacher/AddImages.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using iMasomo;
+using System.Data.SQLite;
+
+
+namespace iMasomo_Teacher
+{
+    /// <summary>
+    /// Interaction logic for AddImages.xaml
+    /// </summary>
+    public partial class AddImages : Page
+    {
+        private SQLiteConnection sqliteConn;
+        public AddImages()
+        {
+            try
+            {
+                InitializeComponent();
+                Database.OpenDatabase();
+                SetDatabaseConnection();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void SetDatabaseConnection()
+        {
+            sqliteConn = Database.GetDatabaseConnection();
+        }
+        private void LoadComboBoxElements()
+        {
+            ObservableCollection<string> list = new ObservableCollection<string>();
+            list.Add("Wanyama na Ndege");
+            list.Add("Sehemu za Mwili");
+            list.Add("Nyumbani");
+            list.Add("Darasani");
+            list.Add("Shuleni");
+            list.Add("Dunai");
+            list.Add("Sokoni");
+            list.Add("Mavazi");
+            list.Add("Vyakula");
+            list.Add("Afya na usafi");
+            list.Add("Maumbo");
+            list.Add("Angani");
+            list.Add("Maswala ya Ibuka");
+            categoryComboBox.ItemsSource = list;
+        }
+        private void browseImagesButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            //set filter for file extension and default file extension
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files(*.jpeg)|*.jpeg|PNG Files(*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+
+            //display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            //get the selected file name and display in a TextBox
+            if(result.HasValue)
+            {
+                //Open document
+                string filename = dlg.FileName;
+                imagePathTextBox.Text = filename;
+            }
+    
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadComboBoxElements();
+        }
+
+        private void addImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string query="insert into image_details(kiswahili_tag,category,path,image_type) values ('"+kiswahiliTagTxtBox.Text.ToLower()+"','"+categoryComboBox.SelectedItem.ToString().ToLower()+"','"+imagePathTextBox.Text+"','user_defined')";
+            try
+            {
+                SQLiteCommand sqliteCommand = new SQLiteCommand(query, sqliteConn);
+                if(sqliteCommand.ExecuteNonQuery()==1)
+                {
+                    MessageBox.Show("Image added to the system");
+                }
+                else
+                {
+                    MessageBox.Show("Error:Image not added");
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ iMasomo/HomePage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+
+namespace iMasomo
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        CreationCenter creationPage = null;
+        MsamiatiPage msamiatiMenu = null;
+        ImlaPage imlaPage = null;
+        KusikilizaPage kusikilizaPage = null;
+        public HomePage()
+        {
+            InitializeComponent();
+            creationPage = new CreationCenter();
+            msamiatiMenu = new MsamiatiPage();
+            imlaPage = new ImlaPage();
+            kusikilizaPage = new KusikilizaPage();
+        }
+
+        private void creationTile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(creationPage);
+        }
+
+        private void mazoeziTile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void kusikilizaTile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(kusikilizaPage);
+        }
+
+        private void msamiatiTile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(msamiatiMenu);
+        }
+
+        private void imlaTile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(imlaPage);
+        }
+    }
+}
